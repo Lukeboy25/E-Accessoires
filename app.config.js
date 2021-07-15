c0b2c97c8f032ed95fc88286f077fe57c84c50da
Luke Spaans <luke-spaans@hotmail.com>
2021-07-15 13:57:09 +0200

    Update version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'E-accessoires',
     slug: 'e-accessoires-app',
-    version: '2.0.0',
+    version: '2.0.1',
     orientation: 'portrait',
     icon: './app/assets/logo-small.png',
     splash: {
