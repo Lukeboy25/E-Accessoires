90bd1ee75c15d21bb4c7fc8ee7b6f3e270874bcd
lukespaans <luke.spaans@create.nl>
2022-02-02 16:00:50 +0100

    Refactor offer to ts

---
+++ app/store/offer/offerActions.ts
@@ -0,0 +1,25 @@
+import { OfferViewModel } from '../../entities/Offer/Offer';
+import HttpService from '../../services/HttpService';
+import { OFFER } from './offerTypes';
+import { Dispatch } from 'redux';
+
+export function setOffer(offer: OfferViewModel) {
+  return {
+    type: OFFER,
+    offer,
+  };
+}
+
+const getOfferByUuid = (offerUuid: string, language: string) => async () => {
+  const httpService = new HttpService(language);
+
+  return await httpService.get(`offers/${offerUuid}`);
+};
+
+export const checkStockForOffer = (offerUuid: string, language: string) => async (dispatch: Dispatch) => {
+  const offer = dispatch(getOfferByUuid(offerUuid, language));
+
+  if (offer.stock.correctedStock === 0) {
+    return `Let op! De voorraad van ${offer.store.productTitle} is opgeraakt!`;
+  }
+};
