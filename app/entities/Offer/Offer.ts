8135dccb246a109173ccb37c8518f02cf520c2a3
lukespaans <luke.spaans@create.nl>
2022-02-07 19:46:33 +0100

    Update OfferViewModel

--- app/entities/Offer/Offer.ts
+++ app/entities/Offer/Offer.ts
@@ -1,3 +1,17 @@
+export interface StockViewModel {
+    amount: number;
+    correctedStock: number;
+    managedByRetailer: boolean;
+}
+
+export interface FulfilmentViewModel {
+    method: string;
+    deliveryCode: string;
+}
+
 export interface OfferViewModel {
-    
+    offerId: string;
+    ean: number;
+    stock: StockViewModel;
+    fulfilment: FulfilmentViewModel;
 }
