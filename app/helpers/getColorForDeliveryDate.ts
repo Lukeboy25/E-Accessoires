35fc3978790b24dfd26734e6eabb4347bac5663a
lukespaans <luke.spaans@create.nl>
2022-04-11 20:55:51 +0200

    Fix BE delivery date.

--- app/helpers/getColorForDeliveryDate.ts
+++ app/helpers/getColorForDeliveryDate.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 
 export const MONDAY = 1;
 
-export const getColorForDeliveryDate = (date, currentDate) => {
+export const getColorForDeliveryDate = (date: string, currentDate: Date) => {
   const newDate = new Date(date);
 
   if (date <= moment(currentDate).format('yyyy-MM-DD')) {
@@ -11,6 +11,31 @@ export const getColorForDeliveryDate = (date, currentDate) => {
   }
 
   if (date === moment(currentDate).add(1, 'days').format('yyyy-MM-DD')) {
+    return styles.dangerText;
+  }
+
+  // Add two days over the weekend
+  if (newDate.getDay() === MONDAY) {
+    if (date <= moment(currentDate).add(3, 'days').format('yyyy-MM-DD')) {
+      return styles.dangerText;
+    }
+  }
+
+  if (date >= moment(currentDate).format('yyyy-MM-DD')) {
+    return styles.safeText;
+  }
+
+  return styles.errorText;
+};
+
+export const getColorForBEDeliveryDate = (date: string, currentDate: Date) => {
+  const newDate = new Date(date);
+
+  if (date <= moment(currentDate).add(1, 'days').format('yyyy-MM-DD')) {
+    return styles.errorText;
+  }
+
+  if (date === moment(currentDate).add(2, 'days').format('yyyy-MM-DD')) {
     return styles.dangerText;
   }
 
