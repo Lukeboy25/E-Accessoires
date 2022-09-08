6bfc7b29248d6adf08acc08e087571b8db83591e
lukespaans <luke.spaans@create.nl>
2022-09-08 17:34:04 +0200

    feat/shipping-label: new version + app updates.

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: 'E-accessoires',
         slug: 'e-accessoires-app',
-        version: '4.1.4',
+        version: '4.1.5',
         orientation: 'portrait',
         icon: './app/assets/logo-new-middle.png',
         splash: {
@@ -23,7 +23,7 @@ export default {
             supportsTablet: true,
         },
         android: {
-            versionCode: 25,
+            versionCode: 26,
             package: 'eaccessoires.app',
             googleServicesFile: './google-services.json',
             adaptiveIcon: {
